31e4f26c6765e2ca9664d23f22c4e4b80cd48dac
Martin Linklater <mslinklater@gmail.com>
2022-06-19 16:02:02 +0100

    debug

--- Debug/DebugUI.gd
+++ Debug/DebugUI.gd
@@ -1,12 +1,19 @@
 extends Control
 
 var bVisible = false;
+onready var gameModeLabel = $GameModeLabel
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	pass
+	match Global.gameMode:
+		Global.GameMode.BOOT:
+			gameModeLabel.text = "GameMode: Boot"
+		Global.GameMode.FRONT_END:
+			gameModeLabel.text = "GameMode: Front End"
+		Global.GameMode.IN_GAME:
+			gameModeLabel.text = "GameMode: In Game"
 
 func _input(event):
 	if event.is_action_pressed("toggle_debug_ui"):
--- global.gd
+++ global.gd
@@ -2,12 +2,37 @@
 
 extends Node
 
+enum GameMode {	BOOT,	FRONT_END,	IN_GAME }
+
+var gameMode setget _set_game_mode
+
+func _set_game_mode(newMode):
+	print("Setting gameMode")
+	gameMode = newMode
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	gameMode = GameMode.BOOT
 	
-#func _process(delta):
-#	pass
+func process_boot():
+	pass
 
+func process_front_end():
+	pass
+
+func process_in_game():
+	pass
+	
+func _process(delta):
+	match gameMode:
+		GameMode.BOOT:
+			process_boot()
+		GameMode.FRONT_END:
+			process_front_end()
+		GameMode.IN_GAME:
+			process_in_game()
+		_:
+			assert(false, "Unhandled gameMode")
+		
 func quit():
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
